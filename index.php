ee43a170ae7830e285426cfb9e8fbaeed5142876
Carter Roeser <carter@cdgtech.one>
2022-08-11 22:26:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 $configlocation = "includes/";
 if (file_exists( 'includes/config.php' )) { require 'includes/includes.php'; }  else { header( 'Location: install' );  exit(); }
 
-if(base64_decode($_SESSION['loggedin']) == 'true') {}
+if(isset($_SESSION['loggedin']) && base64_decode($_SESSION['loggedin']) == 'true') {}
 else { header('Location: login.php'); exit(); }
 
 $postvarsx = array('hash' => $vst_apikey, 'user' => $vst_username,'password' => $vst_password,'cmd' => 'v-list-sys-info','arg1' => 'json');
